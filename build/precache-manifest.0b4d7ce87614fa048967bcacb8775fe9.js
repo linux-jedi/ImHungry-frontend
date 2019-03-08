db752d3d04df3a8026b6b190d167c4acf5346226
Jonathan Chai <chaijw@usc.edu>
2019-03-08 00:23:20 -0800

    adlf;jk

---
+++ build/precache-manifest.0b4d7ce87614fa048967bcacb8775fe9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8635d064d62718af39c3",
+    "url": "/static/js/main.8635d064.chunk.js"
+  },
+  {
+    "revision": "225abd945c80c67716c0",
+    "url": "/static/js/2.225abd94.chunk.js"
+  },
+  {
+    "revision": "8635d064d62718af39c3",
+    "url": "/static/css/main.a7f780b7.chunk.css"
+  },
+  {
+    "revision": "70d319044d3c7c2f13c5edcf77908cc0",
+    "url": "/index.html"
+  }
+];
